d9a4af0e086bbc2407a434e1662fd3e9bfbf20d7
JBOhge <ohgejb@gmail.com>
2022-02-09 15:13:19 -0600

    Base SpringBoot + Web,DevTools,Act

---
+++ src/main/java/com/app/satpoint/SatPointApplication.java
@@ -0,0 +1,13 @@
+package com.app.satpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatPointApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SatPointApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/satpoint/SatPointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.satpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatPointApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
